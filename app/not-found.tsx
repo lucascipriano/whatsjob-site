7d2aa0839b3bd17aefa1779d95ea826ab5b126a2
Eduardo <eduardo.maciel@edu.univali.br>
2024-06-04 21:55:44 -0300

    Create page 404

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen w-full gap-12 justify-center items-center">
+      <Image
+        alt="Imagem 404 de conteúdo não achado"
+        width={600}
+        height={200}
+        priority
+        src={"/images/404.svg"}
+      />
+      <div className="max-w-96 flex flex-col">
+        <h2 className="text-tremor-brand-emphasis text-7xl font-light mb-3">
+          404
+        </h2>
+        <h1 className="text-2xl text-black font-extralight mb-3">
+          Página não encontrada
+        </h1>
+        <p className="text-black mb-3">
+          A página que você procura não existe. Você pode clicar no botão abaixo
+          para voltar à página inicial.
+        </p>
+        <Link href={"/sign-in"}>
+          <Button variant={"default"} className="mt-2">
+            Voltar para o Início
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
