db26345fe8e02db8ced4d72cf5fffbff6aceb4ad
Eduardo Amaro <edu.amr@hotmail.com>
2024-06-30 18:10:55 -0300

    Ajustes do text effect

--- app/_components/TextEffect.tsx
+++ app/_components/TextEffect.tsx
@@ -5,8 +5,13 @@ import { Typewriter } from "react-simple-typewriter";
 
 export function TextEffect({ words }: { words: string[] }) {
   return (
-    <span className="bg-[#5D8A83] px-5 py-2 rounded-sm">
-      <Typewriter words={words} loop />
-    </span>
+    <>
+      <span className="bg-[#5D8A83] px-5 py-2 rounded-sm lg:inline hidden">
+        <Typewriter words={words} loop />
+      </span>
+      <span className="bg-[#5D8A83] px-5 py-2 rounded-sm inline lg:hidden">
+        Vaga
+      </span>
+    </>
   );
 }
